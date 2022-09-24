fe94c0eea4d4e58a7296fa6e5d3de8dcf723e205
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-09-24 13:04:49 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Denis Bokov on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
