37b593beacda563eebaf9a702b70b341bf7d578f
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-26 19:06:23 +0300

    Create Data class

--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -5,4 +5,9 @@
 //  Created by Denis Bokov on 26.06.2024.
 //
 
-import Foundation
+class DataStore {
+    let firstName = ["Викуся", "Кира", "Катюшка", "Милко", "Александр", "Роман"]
+    let lastName = ["Малиновский", "Жданов", "Варопаев", "Клочкова", "Пушкарева", "Шмелева"]
+    let email = ["rybomoya@mail.ru", "babochka@coolclever.ru", "katyushka@bud.net", "andryushka@bud.da", "rodisskrosivoy@ruby.com", "nebudkakkatyushka@gmail.com"]
+    let telephoneNumber = ["89010000000", "89020000001", "89030000002", "89040000003", "89050000004", "89060000005"]
+}
