3a154509e5e591b42e500d907223d97f5f554095
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-27 12:47:00 +0300

    Добавлено табличное представление для первых элементов.

--- ContactList/ViewControllers/PersonListViewController.swift
+++ ContactList/ViewControllers/PersonListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PersonListViewController: UITableViewController {
+class PersonListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
