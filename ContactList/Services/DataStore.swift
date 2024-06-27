528766fee02b8ef904a7eb68b5b08d8f6d3fe959
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-27 19:49:50 +0300

    Rename

--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -6,8 +6,8 @@
 //
 
 class DataStore {
-    let firstName = ["Викуся", "Кира", "Катюшка", "Милко", "Александр", "Роман"]
-    let lastName = ["Малиновский", "Жданов", "Варопаев", "Клочкова", "Пушкарева", "Шмелева"]
-    let email = ["rybomoya@mail.ru", "babochka@coolclever.ru", "katyushka@bud.net", "andryushka@bud.da", "rodisskrosivoy@ruby.com", "nebudkakkatyushka@gmail.com"]
-    let telephoneNumber = ["89010000000", "89020000001", "89030000002", "89040000003", "89050000004", "89060000005"]
+    let firstNames = ["Викуся", "Кира", "Катюшка", "Милко", "Александр", "Роман"]
+    let lastNames = ["Малиновский", "Жданов", "Варопаев", "Клочкова", "Пушкарева", "Шмелева"]
+    let emails = ["rybomoya@mail.ru", "babochka@coolclever.ru", "katyushka@bud.net", "andryushka@bud.da", "rodisskrosivoy@ruby.com", "nebudkakkatyushka@gmail.com"]
+    let telephoneNumbers = ["89010000000", "89020000001", "89030000002", "89040000003", "89050000004", "89060000005"]
 }
