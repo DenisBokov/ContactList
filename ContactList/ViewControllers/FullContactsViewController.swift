059172d250ab5c4cf625f35eef5b1c27e9593fdf
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-07-05 13:18:50 +0300

    First realisition Table with Sections

--- ContactList/ViewControllers/FullContactsViewController.swift
+++ ContactList/ViewControllers/FullContactsViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class FullContactsViewController: UIViewController {
     
-    
     @IBOutlet var tableView: UITableView!
+    
+    private var persons = Person.getPersonContact()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
+}
+
+extension FullContactsViewController: UITableViewDataSource {
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        persons.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        persons[section].fullName
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "fullVisionContacts", for: indexPath)
+        let person = persons[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = person.email
+        content.secondaryText = person.phoneNumber
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    
 }
