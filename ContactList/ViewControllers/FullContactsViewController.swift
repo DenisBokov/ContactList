84a6f3b6089f7a908c8ecec89b584566729c27d2
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-07-05 11:51:24 +0300

    Create Full Contacts View Controller

---
+++ ContactList/ViewControllers/FullContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FullContactsViewController.swift
+//  ContactList
+//
+//  Created by Denis Bokov on 05.07.2024.
+//
+
+import UIKit
+
+class FullContactsViewController: UIViewController {
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
