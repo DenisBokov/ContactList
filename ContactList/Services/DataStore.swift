18206a4c3baeb349f2605f96568b5d84676ad04d
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-07-05 13:17:10 +0300

    A bit of visual order

--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -6,8 +6,39 @@
 //
 
 class DataStore {
-    let firstNames = ["Викуся", "Кира", "Катюшка", "Милко", "Александр", "Роман"]
-    let lastNames = ["Малиновский", "Жданов", "Варопаев", "Клочкова", "Пушкарева", "Шмелева"]
-    let emails = ["rybomoya@mail.ru", "babochka@coolclever.ru", "katyushka@bud.net", "andryushka@bud.da", "rodisskrosivoy@ruby.com", "nebudkakkatyushka@gmail.com"]
-    let telephoneNumbers = ["89010000000", "89020000001", "89030000002", "89040000003", "89050000004", "89060000005"]
+    let firstNames = [
+        "Викуся",
+        "Кира",
+        "Катюшка",
+        "Милко",
+        "Александр",
+        "Роман"
+    ]
+    
+    let lastNames = [
+        "Малиновский",
+        "Жданов",
+        "Варопаев",
+        "Клочкова",
+        "Пушкарева",
+        "Шмелева"
+    ]
+    
+    let emails = [
+        "rybomoya@mail.ru",
+        "babochka@coolclever.ru",
+        "katyushka@bud.net",
+        "andryushka@bud.da",
+        "rodisskrosivoy@ruby.com",
+        "nebudkakkatyushka@gmail.com"
+    ]
+    
+    let telephoneNumbers = [
+        "89010000000",
+        "89020000001",
+        "89030000002",
+        "89040000003",
+        "89050000004",
+        "89060000005"
+    ]
 }
